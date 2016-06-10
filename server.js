09a4d29740b07c27871c7ff38978c1bc23d5e485
Zilvinas Siaulys <zilvinas.siaulys@visma.com>
2016-06-10 08:13:22 +0300

    Hello World

---
+++ server.js
@@ -0,0 +1,20 @@
+var path = require('path');
+var express = require('express');
+var app = express();
+
+// Serve static files from the public and views directories
+app.use("/", express.static(path.join(__dirname, 'src/public')));
+app.use("/", express.static(path.join(__dirname, 'src/views')));
+
+// Return index.html on all requests
+app.get('*', function(req, res) {
+    res.sendFile('index.html', { root: path.join(__dirname, 'src/views') });
+});
+
+// Set up the listening port
+var port = process.env.PORT || 3000;
+
+// Launch the app and listen to requests
+app.listen(port, function () {
+    console.log('App listening on port: ' + port + '!');
+});
